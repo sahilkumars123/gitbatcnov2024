df18a91a81ac566f7302cbb7fa709dda4665a297
Sahil Kumar <sahil.kumar9274@gmail.com>
2024-11-13 05:54:41 +0530

    practicig stasj

--- src/main/java/gitbatchnov2024/PaymentPage.java
+++ src/main/java/gitbatchnov2024/PaymentPage.java
@@ -5,6 +5,16 @@ public class PaymentPage {
 	public static void main(String[] args) {
 
 			System.out.println("this is payment page by dev1");
+			
+			System.out.println("this is doing is stash1");
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+			
+			System.out.println("this is doing in stash2");
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 	}
 
 }
