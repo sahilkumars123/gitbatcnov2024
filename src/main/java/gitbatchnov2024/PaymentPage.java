8aeaa15d0979bd08f027730d89d7eabcfbd3b766
Sahil Kumar <sahil.kumar9274@gmail.com>
2024-11-13 05:36:30 +0530

    Added payment page by dev1

--- src/main/java/gitbatchnov2024/PaymentPage.java
+++ src/main/java/gitbatchnov2024/PaymentPage.java
@@ -4,7 +4,11 @@ public class PaymentPage {
 
 	public static void main(String[] args) {
 
+<<<<<<< Updated upstream
 			System.out.println("this is payment page by dev2");
+=======
+			System.out.println("this is payment page by dev1");
+>>>>>>> Stashed changes
 	}
 
 }
