5cec43bb8e1faddbd7175e1bc5eeadfecbbe8f6e
Sahil Kumar <sahil.kumar9274@gmail.com>
2024-11-13 05:38:27 +0530

    removed one space

--- src/main/java/gitbatchnov2024/PaymentPage.java
+++ src/main/java/gitbatchnov2024/PaymentPage.java
@@ -4,11 +4,7 @@ public class PaymentPage {
 
 	public static void main(String[] args) {
 
-<<<<<<< Updated upstream
-			System.out.println("this is payment page by dev2");
-=======
 			System.out.println("this is payment page by dev1");
->>>>>>> Stashed changes
 	}
 
 }
